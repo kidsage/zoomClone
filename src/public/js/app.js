c8db46854825e5ea5eff599b2fdc061ca5e125c9
Kimseokju <ruhyun92@gmail.com>
2023-11-13 15:57:50 +0900

    + fin to chap 2

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -8,11 +8,39 @@ room.hidden = true;
 
 let roomName;
 
-function showRoom(msg) {
+function addMessage(message) {
+    const ul = room.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerText = message;
+    ul.appendChild(li);
+}
+
+function handleMessageSubmit(event) {
+    event.preventDefault();
+    const input = room.querySelector("#msg input");
+    const value = input.value;
+    socket.emit("new_message", value, roomName, () => {
+        addMessage(`u: ${value}`);
+    });
+    input.value = "";
+}
+
+function handleNicknameSubmit(event) {
+    event.preventDefault();
+    const input = room.querySelector("#name input");
+    const value = input.value;
+    socket.emit("nickname", value);
+}
+
+function showRoom(newCount) {
     welcome.hidden = true;
     room.hidden = false;
     const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName}`;
+    h3.innerText = `Room ${roomName}(${newCount})`;
+    const msgForm = room.querySelector("#msg");
+    const nameForm = room.querySelector("#name");
+    msgForm.addEventListener("submit", handleMessageSubmit);
+    nameForm.addEventListener("submit", handleNicknameSubmit);
 }
 
 function handleRoomSubmit(event) {
@@ -24,3 +52,30 @@ function handleRoomSubmit(event) {
 }
 
 form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", (user, newCount) => {
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName} (${newCount})`;
+    addMessage(`${user} arrived!`);
+});
+
+socket.on("bye", (left, newCount) => {
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName} (${newCount})`;
+    addMessage(`${left} left haha`);
+});
+
+socket.on("new_message", addMessage);
+
+socket.on("room_change", (rooms) => {
+    const roomList = welcome.querySelector("ul");
+    if (rooms.length === 0) {
+        roomList.innerHTML = "";
+        return;
+    }
+    rooms.forEach((room) => {
+        const li = document.createElement("li");
+        li.innerText = room;
+        roomList.append(li);
+    });
+});
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import { createServer } from "http";
 import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 import express from "express";
 import path, { parse } from "path";
 
@@ -12,21 +13,66 @@ app.use("/public", express.static(__dirname + "/src/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on io`);
-
 // http + ws 구성
 const httpServer = createServer(app);
 // es 표기
-const io = new Server(httpServer);
+// admin-ui 설정
+const io = new Server(httpServer, {
+    cors: {
+        origin: ["https://admin.socket.io"],
+        credentials: true,
+    },
+});
+
+instrument(io, {
+    auth: false,
+    mode: "development",
+});
+
+function publicRooms() {
+    const {
+        sockets: {
+            adapter: { sids, rooms },
+        },
+    } = io;
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+        if (sids.get(key) === undefined) {
+            publicRooms.push(key);
+        }
+    });
+    return publicRooms;
+}
+
+function countRoom(roomName) {
+    return io.sockets.adapter.rooms.get(roomName)?.size;
+}
 
 io.on("connection", (socket) => {
+    socket["nickname"] = "Anonymous";
     socket.onAny((event) => {
         console.log(`socket event: ${event}`);
     });
     socket.on("enter_room", (roomName, done) => {
         socket.join(roomName);
+        done(countRoom(roomName)); // callback에서 payload로 보냄으로써 모든 클라이언트에서 업데이트 가능하게끔 작동
+        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
+        io.sockets.emit("room_change", publicRooms());
+    });
+
+    // 연결이 끊어지지만 room에는 있는 상태 disconnet 전단계라고 생각하면 좋을 듯
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)); // 완전 나가기 전이라 -1
+    });
+    socket.on("disconnect", () => {
+        io.sockets.emit("room_change", publicRooms());
+    });
+    socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
         done();
     });
+    socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 });
 
+const handleListen = () => console.log(`Listening on io`);
 httpServer.listen(3000, handleListen);
